5acf7232029b72c7ce87347cc2f6d0cc7fbcc6de
AxellNetwork <axellsuppport@gmail.com>
2025-02-01 12:22:37 +0700

    Fix bug

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,25 +1,13 @@
-const makeWASocket = require('./Socket');
-
-const WAProto = require('../WAProto');
-const Utils = require('./Utils');
-const Types = require('./Types');
-const Store = require('./Store');
-const Defaults = require('./Defaults');
-const WABinary = require('./WABinary');
-const WAM = require('./WAM');
-const WAUSync = require('./WAUSync');
-
-const WASocket = makeWASocket;
-
-module.exports = {
-  ...WAProto,
-  ...Utils,
-  ...Types,
-  ...Store,
-  ...Defaults,
-  ...WABinary,
-  ...WAM,
-  ...WAUSync,
-  WASocket,
-  makeWASocket,
-};
+import { proto } from '../WAProto';
+import makeWASocket from './Socket';
+export * from '../WAProto';
+export * from './Utils';
+export * from './Types';
+export * from './Store';
+export * from './Defaults';
+export * from './WABinary';
+export * from './WAM';
+export * from './WAUSync';
+export type WASocket = ReturnType<typeof makeWASocket>;
+export { makeWASocket, proto };
+export default makeWASocket;
